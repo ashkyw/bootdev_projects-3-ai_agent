de05f6ff5dd1acb51c654d156e004c8d7ed05565
Kyle <ashkyw@gmail.com>
2026-02-03 17:31:20 -0600

    added api call

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
+import os
+
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+if api_key == None:
+    raise RuntimeError("Missing API key")
+
+client = genai.Client(api_key=api_key)
+response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+)
+
+
 def main():
-    print("Hello from bootdev-ai-agent!")
-    t
+    print(response.text)
 
 
 if __name__ == "__main__":
