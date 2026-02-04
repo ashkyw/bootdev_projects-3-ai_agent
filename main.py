a4a8968465109a1fc63f566a91e894decad95bcb
Kyle <ashkyw@gmail.com>
2026-02-03 18:45:14 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,48 @@
+import argparse
 import os
 
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
+# Set API key from user environment
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
+
 if api_key == None:
     raise RuntimeError("Missing API key")
 
+# Set args variable to with argparser
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+args = parser.parse_args()
+
+# Create a list of messages and responses from argparser
+messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
+# Set AI client and response
 client = genai.Client(api_key=api_key)
 response = client.models.generate_content(
     model="gemini-2.5-flash",
-    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+    contents=messages,
 )
+
+# Set metadata variables using response's metadata
+usage_metadata = response.usage_metadata
+prompt_tokens = response.usage_metadata.prompt_token_count
+candidate_tokens = response.usage_metadata.candidates_token_count
 
 
 def main():
+    if usage_metadata == None:
+        raise RuntimeError("API request failed")
+
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {prompt_tokens}")
+        print(f"Response tokens: {candidate_tokens}")
+
     print(response.text)
 
 
