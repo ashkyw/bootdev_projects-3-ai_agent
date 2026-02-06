ab0d73017f431676c4d4bdfb463d4b2e2afc32de
Kyle <ashkyw@gmail.com>
2026-02-05 18:17:57 -0600

    added write_file functions and tests

---
+++ functions/write_file.py
@@ -0,0 +1,42 @@
+import os
+
+
+def write_file(working_directory, file_path, content):
+    try:
+        # get absolute path of working directory
+        absolute_path = os.path.abspath(working_directory)
+    except Exception:
+        return "Error: Invalid working directory"
+    try:
+        # get normalized, joined path of absolute path and directory
+        target_path = os.path.normpath(os.path.join(absolute_path, file_path))
+    except Exception:
+        return "Error: Invalid directory"
+    try:
+        # check if target directory is in the absolute path
+        valid_path = os.path.commonpath([absolute_path, target_path]) == absolute_path
+    except Exception:
+        return "Error: No common path found"
+
+    # Safeguards to prevent LLM from running amok in system
+    if not valid_path:
+        return f"Error: Cannot read {file_path} as it is outside the permitted working directory"
+
+    if os.path.isdir(target_path):
+        return f"Error: Cannot write to {file_path} as it is a directory"
+    try:
+        dir_name, file_name = file_path.split("/")
+        parent_directory = os.path.join(working_directory, dir_name)
+        target_file = os.path.join(parent_directory, file_name)
+    except ValueError:
+        parent_directory = working_directory
+        target_file = os.path.join(working_directory, file_path)
+
+    try:
+        os.makedirs(parent_directory, exist_ok=True)
+        with open(target_file, "w") as f:
+            f.write(content)
+            return f"Successfully wrote to '{file_path}'({len(content)} characters written)"
+
+    except FileNotFoundError:
+        return f"Error: File not found or is not a regular file: '{file_path}'"
