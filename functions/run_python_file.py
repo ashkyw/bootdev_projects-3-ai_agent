0e7bd11d716c0681c1d3f702f6122466ce872d5c
Kyle <ashkyw@gmail.com>
2026-02-05 20:43:55 -0600

    added run_python_files and tests

---
+++ functions/run_python_file.py
@@ -0,0 +1,66 @@
+import os
+import subprocess
+
+
+def run_python_file(working_directory, file_path, args=None):
+    file_name, file_extension = os.path.splitext(file_path)
+
+    if file_extension != ".py":
+        return f'Error: "{file_path}" is not a Python file'
+
+    try:
+        # get absolute path of working directory
+        absolute_path = os.path.abspath(working_directory)
+    except Exception:
+        return "Error: Invalid working directory"
+    try:
+        # get normalized, joined path of absolute path and directory
+        target_path = os.path.normpath(os.path.join(absolute_path, file_path))
+    except Exception:
+        return "Error: Invalid directory"
+    try:
+        # check if target directory is in the absolute path
+        valid_path = os.path.commonpath([absolute_path, target_path]) == absolute_path
+    except Exception:
+        return "Error: No common path found"
+
+    # Safeguards to prevent LLM from running amok in system
+    if not valid_path:
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+
+    if not os.path.isfile(file_path):
+        return f'Error:  "{file_path}" does not exist or is not a regular file'
+
+    try:
+        dir_name, file_name = file_path.split("/")
+        parent_directory = os.path.join(working_directory, dir_name)
+        target_file = os.path.join(parent_directory, file_name)
+
+    except ValueError:
+        parent_directory = working_directory
+        target_file = os.path.join(working_directory, file_path)
+
+    # Build command to execute Python file
+    command = ["python", target_file]
+    if args:
+        command.extend(args)
+
+    # Execute Python file
+    try:
+        completed_process = subprocess.run(
+            command,
+            check=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            # capture_output=True,
+            text=True,
+            timeout=30,
+        )
+    except subprocess.CalledProcessError as e:
+        return f"Error: executing Python File {e}"
+    if completed_process.returncode != 0:
+        return f"Process exited with code {completed_process.returncode}"
+    if not completed_process.stdout and not completed_process.stderr:
+        return "No output produced"
+
+    return f"STDOUT:{completed_process.stdout} STDERR: {completed_process.stderr}"
